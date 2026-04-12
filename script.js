9022fe7d505de6e53f86716023422c9409deb489
hafijshaikh <officialhafijshaikh@gmail.com>
2026-04-13 02:13:52 +0530

    Refactor API key injection in script.js
    Refactor key injection logic to replace existing key placeholder with the actual API key.

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
 const fs = require('fs');
-const path = require('path');
 const apiKey = process.env.OPENKEY;
 if (!apiKey) { console.error('Error: OPENKEY secret is missing.'); process.exit(1); }
 const p = path.join(__dirname, 'app.js');
 let a = fs.readFileSync(p, 'utf8');
-var lines = a.split('\n');
-var found = false;
-for (var i = 0; i < lines.length; i++) {
-  if (lines[i].indexOf('KEY_PH') !== -1 && lines[i].indexOf('=') !== -1) {
-    lines[i] = "var KEY_PH = '" + apiKey.replace(/'/g, "\\'") + "';";
-    found = true;
-    break;
-  }
-}
-if (!found) { console.error('Error: KEY_PH line not found in app.js'); process.exit(1); }
-fs.writeFileSync(p, lines.join('\n'));
+a = a.replace(/\/\/__KEY__[\s\S]*?\/\/__ENDK__\n?/g, '');
+a = '//__KEY__\nvar __INJ_KEY__ = "' + apiKey.replace(/"/g, '\\"') + '";\n//__ENDK__\n' + a;
+fs.writeFileSync(p, a);
 console.log('Key injected. Length: ' + apiKey.length);
