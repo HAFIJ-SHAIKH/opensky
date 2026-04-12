45103336367ff1e7edd2ba16fe6a041cdc498df8
hafijshaikh <officialhafijshaikh@gmail.com>
2026-04-13 02:22:35 +0530

    Inject API key into app.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
+const path = require('path');
 const apiKey = process.env.OPENKEY;
 if (!apiKey) { console.error('Error: OPENKEY secret is missing.'); process.exit(1); }
 const p = path.join(__dirname, 'app.js');
 let a = fs.readFileSync(p, 'utf8');
-a = a.replace(/\/\/__KEY__[\s\S]*?\/\/__ENDK__\n?/g, '');
-a = '//__KEY__\nvar __INJ_KEY__ = "' + apiKey.replace(/"/g, '\\"') + '";\n//__ENDK__\n' + a;
+a = a.replace("var KEY_PH = 'NONE';", "var KEY_PH = '" + apiKey + "';");
 fs.writeFileSync(p, a);
 console.log('Key injected. Length: ' + apiKey.length);
